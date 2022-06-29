11e08519421ec87abae0c45ef93c489265a0b646
Servio <servio.mora@bvacommerce.com>
2022-06-28 21:38:15 -0400

    simple setup

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <h2>Task Entry Project</h2>
+  </header>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Header',
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped></style>
